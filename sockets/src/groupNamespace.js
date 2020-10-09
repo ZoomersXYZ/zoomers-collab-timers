065ce660945fa770f0c9d070ef865fa62179a4da
Saadh S <atextbooksituation@gmail.com>
2020-10-09 08:33:20 -0400

    Accidentally had copy pasted code in file

--- sockets/src/groupNamespace.js
+++ sockets/src/groupNamespace.js
@@ -17,51 +17,9 @@ lastGetLog = new Date().getTime();
 
 confirmIdPong = false;
 
-seshie = {};
 seshieParent = {};
+seshie = {};
 sassy = {};
-
-function deleteCollection(db, nspName ) {
-  let query = db
-    .where('group', '<', nspName )
-    .orderBy( 'timestamp', 'desc' );
-
-  return new Promise((resolve, reject) => {
-    deleteQueryBatch(db, query, resolve, reject);
-  });
-}
-
-function deleteQueryBatch(db, query, resolve, reject) {
-  query.get()
-    .then((snapshot) => {
-      // When there are no documents left, we are done
-      if (snapshot.size === 0) {
-        return 0;
-      }
-
-      // Delete documents in a batch
-      let batch = db.batch();
-      snapshot.forEach( (thing) => {
-        batch.delete(thing.ref);
-      });
-
-      return batch.commit().then(() => {
-        return snapshot.size;
-      });
-    }).then((numDeleted) => {
-      if (numDeleted === 0) {
-        resolve();
-        return;
-      }
-
-      // Recurse on the next process tick, to avoid
-      // exploding the stack.
-      process.nextTick(() => {
-        deleteQueryBatch(db, query, resolve, reject);
-      });
-    })
-    .catch(reject);
-}
 
 ////
 // Socket
