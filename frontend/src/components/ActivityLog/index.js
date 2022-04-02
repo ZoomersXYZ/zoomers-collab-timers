931091e3e0f0eef58595732428842bdde21a03bc
Saadh S <atextbooksituation@gmail.com>
2022-04-02 18:33:39 -0400

    Messed this up. was checking in index2 i believe. so this is including tons of changes when many were done before

--- frontend/src/components/ActivityLog/index.js
+++ frontend/src/components/ActivityLog/index.js
@@ -2,25 +2,77 @@ import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import { Collapse } from 'react-collapse';
+import { formatRelative, sub } from 'date-fns';
+import { collection, onSnapshot, limit, query, where, orderBy } from "firebase/firestore";
+
+import db from './../../config/firebase';
 
 import Activity from './../Activity';
 import './styles.scss';
 
-import { GroupContext } from '../Contexts';
+import { GroupContext, RoomContext } from '../Contexts';
 
-const ActivityLog = ( { timer, userEnabled } ) => {
+const ActivityLog = ( { userEnabled } ) => {
   const { gName } = useContext( GroupContext );
+  const __room = useContext( RoomContext );
+  const timer = __room ? __room.name : null;
+
+  // timer prop - but it is only around when 
   const [ localLog, setLog ] = useState( [] );
   const [ toSlice, setSlice ] = useState( -8 );
+
   useEffect( () => {
+    // Core
+    const d = new Date();
+    const aYearAgo = sub( d, {
+      days: 365 
+    } ).getTime();
+
+    const rootRef = collection( db, 'groups', gName, 'log' );
+    let ref;
     if ( timer ) {
-      const slicedLog = log.filter( primer => primer.timer === timer );
-      setLog( slicedLog.slice( -10 ) );
+
+      ref = query( 
+        rootRef, 
+        where( 'timer', '==', timer ), 
+        where( 'timestamp', '>', aYearAgo ), 
+        orderBy( 'timestamp', 'desc' ), 
+        limit( 15 ) 
+      );
       setSlice( -5 );
+
     } else {
-      setLog( log.slice( -20 ) );
+
+      ref = query( 
+        rootRef, 
+        where( 'timestamp', '>', aYearAgo ), 
+        orderBy( 'timestamp', 'desc' ), 
+        limit( 40 ) 
+      );
+      setSlice( -8 );
+
     };
-  }, [ log, timer ] );
+
+    const stream = onSnapshot( 
+      ref, 
+      doc => {
+        const arr = [];
+        const current = new Date();
+        doc.forEach( solo => { 
+          const data = solo.data();
+          data.formattedTime = formatRelative( data.timestamp, current );
+          arr.push( data );
+        } );
+        arr.reverse();
+        setLog( arr );
+      }, 
+      err => {
+        console.log( err );
+      } 
+    );
+    return () => stream();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [] );
 
   const [ open, setOpen ] = useState( false );
   const handleCollapse = () => setOpen( prevState => !prevState );
@@ -41,7 +93,7 @@ const ActivityLog = ( { timer, userEnabled } ) => {
       </div>
       <div id="activity-log">
         <Collapse isOpened={ open }>
-          { localLog.slice( 0, toSlice ).map( ( arrival, index ) => 
+          {/* { localLog.slice( 0, toSlice ).map( ( arrival, index ) => 
             <Activity
               key={ `full-log-${ index }` } 
               name={ arrival.username } 
@@ -51,10 +103,23 @@ const ActivityLog = ( { timer, userEnabled } ) => {
               activity={ arrival.activity } 
               formattedTime={ arrival.formattedTime } 
             />
+          ) } */}
+          { localLog.slice( 0, toSlice ).map( ( { username, email, group, timer, activity, formattedTime }, index ) => 
+            <Activity
+              key={ `full-log-${ index }` } 
+              name={ username } 
+              { ...{ 
+                email, 
+                group, 
+                timer, 
+                activity, 
+                formattedTime 
+              } } 
+            />
           ) }
         </Collapse>
 
-        { localLog.slice( toSlice ).map( ( arrival, index ) => 
+        {/* { localLog.slice( toSlice ).map( ( arrival, index ) => 
           <Activity
             key={ `log-clip-${ index }` } 
             name={ arrival.username } 
@@ -63,6 +128,19 @@ const ActivityLog = ( { timer, userEnabled } ) => {
             timer={ arrival.timer } 
             activity={ arrival.activity } 
             formattedTime={ arrival.formattedTime } 
+          />
+        ) } */}
+        { localLog.slice( toSlice ).map( ( { username, email, group, timer, activity, formattedTime }, index ) => 
+          <Activity
+            key={ `log-clip-${ index }` } 
+            name={ username } 
+            { ...{ 
+              email, 
+              group, 
+              timer, 
+              activity, 
+              formattedTime 
+            } } 
           />
         ) }
       </div>
@@ -75,17 +153,11 @@ const ActivityLog = ( { timer, userEnabled } ) => {
 };
 
 ActivityLog.propTypes = {
-  log: PropTypes.array.isRequired, 
-  timer: PropTypes.string, 
-  userEnabled: PropTypes.bool, 
-  group: PropTypes.string 
+  userEnabled: PropTypes.bool 
 };
 
 ActivityLog.defaultProps = { 
-  log: [], 
-  timer: null, 
-  userEnabled: false, 
-  group: null 
+  userEnabled: false 
 };
 
 export default ActivityLog;
