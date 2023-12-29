d6cbd2c61b1bc66d418ab59833a6ba22ca0166d8
Saadh S <atextbooksituation@gmail.com>
2023-12-29 17:05:13 -0500

    Logs again

--- sockets/src/core/SharedAndUtil.js
+++ sockets/src/core/SharedAndUtil.js
@@ -45,21 +45,21 @@ const SharedAndUtil = function (
     // Fine if room is null too
     if ( !isEmpty( data ) && typeof data === 'object' ) {
       // nspaceEmit( msg, { room, ...data } );
-      // l.bbc.debug( `22 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
+      l.bbc.debug( `22 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
       socket.to( `${ nspName }-${ room }` ).emit( msg, { room, ...data } );
       socket.emit( msg, { room, ...data } );
     // just room, no data
     } else if( isEmpty( data ) && !isEmpty( room ) ) {
-      // l.bbc.debug( `33 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
+      l.bbc.debug( `33 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
       socket.to( `${ nspName }-${ room }` ).emit( msg, room );
       socket.emit( msg, room );
     // no room or data. plain.
     } else if ( isEmpty( room ) && !isEmpty(data) ) {
-      // l.bbc.debug( `44 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
+      l.bbc.debug( `44 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
       socket.to(nspName).emit(msg, data);
       socket.emit(msg, data);
     } else if ( isEmpty( room ) && isEmpty( data) ) {
-      // l.bbc.debug( `55 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
+      l.bbc.debug( `55 nspName: ${ nspName }, room: ${ room }, msg: ${ msg }` );
       socket.to(nspName).emit(msg)
       socket.emit(msg);
     };
